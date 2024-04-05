9f91e74a2ff816e21307fc6c8a6a2da3947dfecf
dhruv mishra <dhruv35m@gmail.com>
2024-04-06 03:16:51 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import "./app.scss";
+const App = () => {
+  return (
+    <div>
+      <section>
+        <a href="#">Home</a>
+      </section>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Porallax</section>
+      <section>Contact</section>
+    </div>
+  );
+};
+
+export default App;
